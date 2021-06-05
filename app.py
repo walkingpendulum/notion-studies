8e1eedefaee1a6690fd7ea906a296abf6ff8e200
walkingpendulum <oayunin@gmail.com>
2021-06-05 23:23:21 +0300

    hello world with picture

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import aiohttp_jinja2
 import jinja2
 from aiohttp import web
 
-here = Path(__file__).resolve().parent / "templates"
+here = Path(__file__).resolve().parent / "static"
 
 
 @aiohttp_jinja2.template('hello.html')
@@ -16,6 +16,8 @@ async def index_handler(request):
 if __name__ == '__main__':
     app = web.Application()
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(str(here)))
+
     app.router.add_get('/', index_handler)
+    app.router.add_static('/images', path='static/images', name='images')
 
     web.run_app(app, port=int(os.getenv("PORT", "8080")))
