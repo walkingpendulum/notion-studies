6032166ec3d6028975666d679b42ac7fc988dda8
walkingpendulum <oayunin@gmail.com>
2021-06-05 22:56:25 +0300

    templates

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
+from pathlib import Path
 
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
 
+here = Path(__file__).resolve().parent / "templates"
 
-async def hello(request):
-    return web.Response(text="Hello, world")
+
+@aiohttp_jinja2.template('hello.html')
+async def index_handler(request):
+    return {'name': 'Andrew'}
 
 
 if __name__ == '__main__':
     app = web.Application()
-    app.router.add_get('/', hello)
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(str(here)))
+    app.router.add_get('/', index_handler)
 
     web.run_app(app, port=int(os.getenv("PORT", "8080")))
